98f6a97afcba46e0ec588a7339bce7821e6b9cdc
Rafal Wilinski <raf.wilinski@gmail.com>
2018-11-30 10:54:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Serverless Puppeteer using AWS Lambda Layers
+
+Recently AWS introduced [Layers](https://aws.amazon.com/about-aws/whats-new/2018/11/aws-lambda-now-supports-custom-runtimes-and-layers/) which enables sharing common code between functions and working with large dependencies (such as headless chrome) much easier.
+
+This project example returns a screenshot of page requested via `?address=` query parameter.
+
+### Usage
+```sh
+serverless deploy
+
+curl <DEPLOYED_ENDPOINT>?address=https://google.com
+```
+
---
+++ handler.js
@@ -0,0 +1,30 @@
+'use strict';
+const fs = require('fs');
+const puppeteer = require('puppeteer');
+
+module.exports.index = async (event, context) => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    executablePath: '/opt/headless_shell',
+    args: ['--no-sandbox', '--disable-gpu', '--single-process'],
+  });
+
+  const page = await browser.newPage();
+  await page.goto(event['queryStringParameters'].address, {
+    waitUntil: ['domcontentloaded', 'networkidle0'],
+  });
+
+  const image = await page.screenshot({
+    clip: { x: 0, y: 0, width: 1024, height: 800 },
+    encoding: 'base64'
+  });
+
+  return {
+    statusCode: 200,
+    body: image,
+    headers: {
+      'Content-Type': 'image/png',
+    },
+    isBase64Encoded: true
+  };
+};
