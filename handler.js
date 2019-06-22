2e1ad8e99b947f045e905ca71da867f16a1d8f8c
Rafal Wilinski <raf.wilinski@gmail.com>
2019-06-22 12:02:47 +0200

    Wrap functionality into try/catch block

--- handler.js
+++ handler.js
@@ -2,30 +2,37 @@
 const puppeteer = require('puppeteer');
 
 module.exports.index = async (event, context) => {
-  const browser = await puppeteer.launch({
-    headless: true,
-    executablePath: '/opt/headless_shell',
-    args: ['--no-sandbox', '--disable-gpu', '--single-process'],
-  });
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+      executablePath: '/opt/headless_shell',
+      args: ['--no-sandbox', '--disable-gpu', '--single-process'],
+    });
 
-  const page = await browser.newPage();
-  await page.goto(event['queryStringParameters'].address, {
-    waitUntil: ['domcontentloaded', 'networkidle0'],
-  });
+    const page = await browser.newPage();
+    await page.goto(event['queryStringParameters'].address, {
+      waitUntil: ['domcontentloaded', 'networkidle0'],
+    });
 
-  const image = await page.screenshot({
-    clip: { x: 0, y: 0, width: 1024, height: 800 },
-    encoding: 'base64'
-  });
+    const image = await page.screenshot({
+      clip: { x: 0, y: 0, width: 1024, height: 800 },
+      encoding: 'base64'
+    });
 
-  browser.close();
+    browser.close();
 
-  return {
-    statusCode: 200,
-    body: image,
-    headers: {
-      'Content-Type': 'image/png',
-    },
-    isBase64Encoded: true
-  };
+    return {
+      statusCode: 200,
+      body: image,
+      headers: {
+        'Content-Type': 'image/png',
+      },
+      isBase64Encoded: true
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500
+    };
+  }
 };
